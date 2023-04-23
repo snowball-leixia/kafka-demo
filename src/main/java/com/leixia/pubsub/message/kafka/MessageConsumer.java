b8f304e9fa92e31d48eeb07112457bf180252b32
Lei Xia <xlfm2@hotmail.com>
2023-04-23 19:40:42 +0100

    testing

--- src/main/java/com/leixia/pubsub/message/kafka/MessageConsumer.java
+++ src/main/java/com/leixia/pubsub/message/kafka/MessageConsumer.java
@@ -1,16 +1,16 @@
 package com.leixia.pubsub.message.kafka;
 
 
-import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
-@Slf4j
 @Component
 public class MessageConsumer {
-
+  private static Logger logger = LoggerFactory.getLogger(MessageConsumer.class);
   private final KafkaTemplate<String, String> kafkaTemplate;
 
   public MessageConsumer(KafkaTemplate<String, String> kafkaTemplate) {
@@ -19,7 +19,7 @@ public class MessageConsumer {
 
   @KafkaListener(topics = {"price-topic"})
   public void consume(ConsumerRecord<String, String> consumerRecord) {
-    log.info("message received {}", consumerRecord.value());
+    logger.info("message received {}", consumerRecord.value());
     kafkaTemplate.send("outbound-price-topic", "bye");
   }
 }
--- src/test/java/com/leixia/pubsub/message/MessageConsumerIntegrationTest.java
+++ src/test/java/com/leixia/pubsub/message/MessageConsumerIntegrationTest.java
@@ -1,9 +1,14 @@
 package com.leixia.pubsub.message;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.leixia.pubsub.message.kafka.MessageConsumer;
 import com.leixia.pubsub.message.kafka.model.PriceMessage;
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -13,10 +18,14 @@ import java.util.concurrent.TimeUnit;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
+import org.mockito.ArgumentCaptor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.test.util.ReflectionTestUtils;
 
 @SpringBootTest(properties = {"spring.kafka.listener.missing-topic-fatal=false"})
 @EmbeddedKafka(
@@ -28,19 +37,29 @@ class MessageConsumerIntegrationTest {
 
   @Autowired
   private KafkaTemplate<String, String> kafkaTemplate;
-
-  private BlockingQueue<ConsumerRecord<String, String>> messages = new LinkedBlockingQueue<>();
+  @Autowired
+  private MessageConsumer messageConsumer;
+  private final BlockingQueue<ConsumerRecord<String, String>> messages = new LinkedBlockingQueue<>();
 
   @Test
   void shouldConsumeMessage() throws InterruptedException, JsonProcessingException {
+    Logger spiedLogger = spy(LoggerFactory.getLogger(MessageConsumer.class));
+    ReflectionTestUtils.setField(messageConsumer, "logger", spiedLogger);
+
     PriceMessage priceMessage = priceMessageFixture();
     ObjectMapper objectMapper = new ObjectMapper();
+
     objectMapper.registerModule(new JavaTimeModule());
     objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
 
     kafkaTemplate.send("price-topic", objectMapper.writeValueAsString(priceMessage));
+    messages.poll(10, TimeUnit.SECONDS);
 
-    ConsumerRecord<String, String> message = messages.poll(10, TimeUnit.SECONDS);
+    ArgumentCaptor<String> logMessageCaptor = ArgumentCaptor.forClass(String.class);
+
+    verify(spiedLogger).info(logMessageCaptor.capture());
+
+    assertThat(logMessageCaptor.getValue()).isNotBlank();
   }
 
   private PriceMessage priceMessageFixture() {
