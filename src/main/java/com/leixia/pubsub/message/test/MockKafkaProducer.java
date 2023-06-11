3e7b119e7c65cf28721672b5c39a3f09b6491884
Lei Xia <xlfm2@hotmail.com>
2023-06-11 12:41:46 +0100

    add docker-compose. add mock message producer

---
+++ src/main/java/com/leixia/pubsub/message/test/MockKafkaProducer.java
@@ -0,0 +1,29 @@
+package com.leixia.pubsub.message.test;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+@Configuration
+@EnableScheduling
+public class MockKafkaProducer {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+    private final ObjectMapper objectMapper;
+
+    public MockKafkaProducer(KafkaTemplate<String, String> kafkaTemplate, ObjectMapper objectMapper) {
+        this.kafkaTemplate = kafkaTemplate;
+        this.objectMapper = objectMapper;
+    }
+
+    @Scheduled(fixedRate = 10000)
+    public void publishMessageOnFixRate() {
+        final String topic = "price-topic";
+        kafkaTemplate.send("{}", topic);
+    }
+
+
+}
