599b0d89238d1c0272d7b933961b77bd7222f25c
Lei Xia <xlfm2@hotmail.com>
2023-07-09 11:36:18 +0100

    add togglz

--- src/main/java/com/leixia/pubsub/message/kafka/properties/KafkaProperties.java
+++ src/main/java/com/leixia/pubsub/message/kafka/properties/KafkaProperties.java
@@ -5,7 +5,7 @@ import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+@Configuration(value = "kafkaProperties")
 @ConfigurationProperties(prefix = "kafka.listener")
 public class KafkaProperties {
 
