78155e49cb0085a456b6348bf52114926fc68dbf
Lei Xia <xlfm2@hotmail.com>
2024-05-12 13:48:59 +0100

    improve line.sh

--- line.sh
+++ line.sh
@@ -2,9 +2,13 @@
 
 set -e
 
-commits=$(git cherry -v main | tr -d '\n')
-length=${#commits}-1
-IFS='+' read -ra array <<< "${commits:1:$length}"
-for line in "${array[@]}"; do
-    echo $line;
-done
+function getCommitMessagesFromBranch() {
+    commits=$(git cherry -v --abbrev main | tr -d '\n')
+    length=${#commits}-1
+    IFS='+' read -ra array <<< "${commits:1:$length}"
+    for line in "${array[@]}"; do
+        echo ${line:9:${#line}};
+    done
+}
+
+getCommitMessagesFromBranch
